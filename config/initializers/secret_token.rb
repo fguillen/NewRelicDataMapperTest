6913c60ab9679f862ab4d417941a7aa086f7944c
Fernando Guillen <fguillen.mail@gmail.com>
2011-12-07 15:29:37 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewRelicDataMapperTest::Application.config.secret_token = '725199d119168c1326bbbb63648f46f5a12b08b76d5e4b2c7fa4197e57bd8d9ac36ef659723106c55b28f41457d529f2e3dd210423dedc6703a6d34b471d4a2b'
